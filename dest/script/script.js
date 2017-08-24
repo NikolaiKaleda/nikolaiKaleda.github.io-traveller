8646685f9b99ea8bbf9099c918d3960c2dc8f276
home\Nikolai <nikolai.kaleda@gmail.com>
2017-08-24 17:30:57 +0300

    Media queries

--- dest/script/script.js
+++ dest/script/script.js
@@ -7,18 +7,36 @@ $(document).ready(function() {
 });
 
 
-$( document ).ready(function() {
-    
-    
+$(document).ready(function () {
+
+
     //ADD LETTER TO BACKGROUND (Content text section)
-    
+
     var backgroundLetter = $('.contentSectionText').text();
     $('.backgroundLetter').text(backgroundLetter.charAt(0));
-    
+
     //End add letter to background
-   
+
+
     
+
 });
+//ADD CLASS TO BUTTONS ( Calendar section )
+
+    /*function addClassSizeScreen() {
+        console.log($(window).width());
+        if ($(window).width() <= '900' ) {
+            $('.ui-corner-all .ui-icon .fa').addClass('fa-5x');
+            console.log('agaewgaeaegaeg');
+        }else {
+            $('.ui-corner-all .ui-icon .fa').removeClass('fa-5x');
+        }
+    }
+    
+    $(window).load(addClassSizeScreen);
+    $(window).resize(addClassSizeScreen);
+*/
+    //End add class to buttons 
 $(window).load(function() {
     $('#slider').nivoSlider({
         effect: 'fade', // Выберите эффекты: 'fold,fade,sliceDown'
